0d375c02a0f69a09eb06d37d827314d106540e32
matteo <matteo@matteo-MS-7253matteo.com>
2013-05-22 13:46:08 +0200

    pulito il main

--- main.c
+++ main.c
@@ -12,12 +12,9 @@ int main()
 {
   int num;
   int a;
-  printf("Commento nel master\n");
-  printf("Commento scritto sul ramo secondario\n");
-  printf("\n");
   printf("PREMI:\n1) per entrare nel primo programma\n2) per entrare nel secondo programma\n3) per entrare nel terzo programma\n0) per uscure\n\n");
-printf("Inserisci un numero: ");
- scanf("%d",&num);
+  printf("Inserisci un numero: ");
+  scanf("%d",&num);
   while (num!=0){
     if(num==1)
       a=primo();
